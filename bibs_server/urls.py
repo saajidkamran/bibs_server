e98b5ea192436c664d90f7713626293866bffe31
saajidkamran <saajidkamran@gmail.com>
2025-04-06 14:56:51 +0530

    added iages pload tro seperate jobimage table

--- bibs_server/urls.py
+++ bibs_server/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from django.conf import settings
+from django.conf.urls.static import static
 from bibs.views import (
     CashCustomerViewSet,
     JobImageViewSet,
@@ -69,4 +71,4 @@ router.register(r"payment-types", NPaymentTypeViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
